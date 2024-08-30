13530a3842fea3e5cb67ad91cdb55c388a4ac9b2
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-30 15:49:47 +0200

    Add asyncWrapper middleware and refactor controllers

--- controllers/aliens.js
+++ controllers/aliens.js
@@ -1,68 +1,49 @@
 const Alien = require('../models/alien');
+const asyncWrapper = require('../middleware/async');
 
-const getAllAliens = async (req, res) => {
-  try {
-    const aliens = await Alien.find({});
-    res.status(200).json({ aliens });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+const getAllAliens = asyncWrapper(async (req, res) => {
+  const aliens = await Alien.find({});
+  res.status(200).json({ aliens });
+});
 
-const createAlien = async (req, res) => {
-  try {
-    const alien = await Alien.create(req.body);
-    res.status(201).json({ alien });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+const createAlien = asyncWrapper(async (req, res) => {
+  const alien = await Alien.create(req.body);
+  res.status(201).json({ alien });
+});
 
-const getAlien = async (req, res) => {
-  try {
-    const { id: alienID } = req.params;
-    const alien = await Alien.findOne({ _id: alienID });
-    if (alien) {
-      res.status(200).json({ alien });
-    } else {
-      res.status(404).json({ msg: `No alien with id: ${alienID}` });
-    }
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const getAlien = asyncWrapper(async (req, res) => {
+  const { id: alienID } = req.params;
+  const alien = await Alien.findOne({ _id: alienID });
+  if (alien) {
+    res.status(200).json({ alien });
+  } else {
+    res.status(404).json({ msg: `No alien with id: ${alienID}` });
   }
-};
+});
 
-const updateAlien = async (req, res) => {
-  try {
-    const { id: alienID } = req.params;
-    const { body } = req;
-    const alien = await Alien.findOneAndUpdate({ _id: alienID }, body, {
-      new: true,
-      runValidators: true,
-    });
-    if (alien) {
-      res.status(200).json({ alien });
-    } else {
-      res.status(404).json({ msg: `No alien with id: ${alienID}` });
-    }
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const updateAlien = asyncWrapper(async (req, res) => {
+  const { id: alienID } = req.params;
+  const { body } = req;
+  const alien = await Alien.findOneAndUpdate({ _id: alienID }, body, {
+    new: true,
+    runValidators: true,
+  });
+  if (alien) {
+    res.status(200).json({ alien });
+  } else {
+    res.status(404).json({ msg: `No alien with id: ${alienID}` });
   }
-};
+});
 
-const deleteAlien = async (req, res) => {
-  try {
-    const { id: alienID } = req.params;
-    const alien = await Alien.findOneAndDelete({ _id: alienID });
-    if (alien) {
-      res.status(200).json({ alien });
-    } else {
-      res.status(404).json({ msg: `No alien with id: ${alienID}` });
-    }
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const deleteAlien = asyncWrapper(async (req, res) => {
+  const { id: alienID } = req.params;
+  const alien = await Alien.findOneAndDelete({ _id: alienID });
+  if (alien) {
+    res.status(200).json({ alien });
+  } else {
+    res.status(404).json({ msg: `No alien with id: ${alienID}` });
   }
-};
+});
 
 module.exports = {
   getAllAliens,
