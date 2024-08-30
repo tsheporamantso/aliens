4557d5a6b7a87012af39ffac0b5a50ac55dcfb52
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-30 12:03:45 +0200

    Add start function with try and catch block that return connectDB promise.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const aliens = require('./routes/aliens');
-require('./db/connect');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 const app = express();
 
@@ -12,4 +13,13 @@ app.use('/api/v1/aliens', aliens);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port} `));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port} `));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
