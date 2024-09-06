6912b24ca26e7d5179ddc5fc37bdb1b8e21d2d46
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-06 10:25:08 +0200

    Add timestamps option object on model

--- models/alien.js
+++ models/alien.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
 
-const AlienSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, 'Must provide name'],
-    maxLength: [20, 'Name cannot be more than  20 characters'],
+const AlienSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Must provide name'],
+      maxLength: [20, 'Name cannot be more than  20 characters'],
+    },
+    tech_Stack: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isEmployed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  tech_Stack: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  isEmployed: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('Alien', AlienSchema);
