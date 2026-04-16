ca9548190e3b83e2c9cd181752a6f3e3aa6c1f90
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-16 22:43:42 +0200

    feat(controllers): add mongoose instance method to compares passwords

--- models/User.js
+++ models/User.js
@@ -40,4 +40,9 @@ userSchema.methods.createJWT = function () {
   );
 };
 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
 module.exports = mongoose.model('User', userSchema);
