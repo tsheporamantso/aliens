e2bb4d175c37c9a0408f37c5a0aac0138db7fc9d
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-16 18:13:28 +0200

    chore: correct extra line linter error

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,7 +1,5 @@
 const StatusCodes = require('http-status-codes');
-
 const CustomErrorAPI = require('./custom-error');
-
 class NotFound extends CustomErrorAPI {
   constructor(message, statusCode = StatusCodes.BAD_REQUEST) {
     super(message, statusCode);
