2fabd092c8c6bd47ecf3676b97895cb194ecd20f
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-16 21:34:55 +0200

    feat(controllers): hashed user passowrd and generate jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,13 @@
 const asyncWrapper = require('../middleware/async');
 const StatusCodes = require('http-status-codes');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const register = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'register user' });
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 });
 
 const login = asyncWrapper(async (req, res) => {
