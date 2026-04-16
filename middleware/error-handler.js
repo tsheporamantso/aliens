e0dcee3b03eaa2d5828930bb2c71cdc549d92823
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-16 11:47:19 +0200

    refactor(middleware): update error handler middleware and check instance of Custome error class

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,10 @@
 const StatusCodes = require('http-status-codes');
+const CustomErrorAPI = require('../errors/custom-error');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomErrorAPI) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
   if (err.name === 'CastError') {
     return res
       .status(StatusCodes.NOT_FOUND)
