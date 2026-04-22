cc46c385c339aff74fd133847f5c8dff5e085af1
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-22 12:54:29 +0200

    refactor(models): add timestamps on User schema

--- models/User.js
+++ models/User.js
@@ -3,36 +3,39 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide name'],
-    minLength: 3,
-    maxLength: 50,
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide email'],
-    validate: {
-      validator: validator.isEmail,
-      message: 'Please provide a valid email',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide name'],
+      minLength: 3,
+      maxLength: 50,
     },
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide password'],
-    minLength: 6,
-  },
-  role: {
-    type: String,
-    enum: {
-      values: ['admin', 'user'],
-      message: '{VALUE} is not supported',
+    email: {
+      type: String,
+      required: [true, 'Please provide email'],
+      validate: {
+        validator: validator.isEmail,
+        message: 'Please provide a valid email',
+      },
+      unique: true,
     },
-    default: 'user',
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      minLength: 6,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ['admin', 'user'],
+        message: '{VALUE} is not supported',
+      },
+      default: 'user',
+    },
   },
-});
+  { timestamps: true },
+);
 
 userSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
