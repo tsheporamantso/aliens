182d58dcff5e53b27cee1c95cd184234bb24840f
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-23 21:47:08 +0200

    chore(cookies): update cookies helper function and add sameSite option

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,11 +1,13 @@
 const attachCookiesToResponse = (response, token) => {
   const oneDay = 1000 * 60 * 60 * 24;
+  const isProduction = process.env.NODE_ENV === 'production';
 
   return response.cookie('token', token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production',
+    secure: isProduction,
     signed: true,
+    sameSite: isProduction ? 'none' : 'lax',
   });
 };
 
